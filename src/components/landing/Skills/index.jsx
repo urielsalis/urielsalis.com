82de977af5ccfc37cfd400131b3d5b13f3a96aa6
Uriel Salischiker <uriel.salischiker@zopa.com>
2020-06-07 17:34:16 +0200

    Customize

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -8,13 +8,14 @@ export const Skills = () => (
   <Wrapper id="about">
     <SkillsWrapper as={Container}>
       <Thumbnail>
-        <img src={dev} alt="I’m John and I’m a Backend & Devops engineer!" />
+        <img src={dev} alt="I’m Uriel and I’m a Kotlin Software Engineer!" />
       </Thumbnail>
       <Details>
         <h1>Hi There!</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
+          Kotlin and Java developer of web and desktop applications using different technologies. Specialized in backend development but experienced with frontend frameworks like Angular and React and mobile app development with Java, Kotlin and Flutter
+          Worked on the travel and fintech sectors and consulted for different businesses, adding to my experience as a volunteer for different projects, including working along with both volunteers and employees of big companies like Microsoft(for Minecraft)
+          Always looking for ways to improve and learn new things!
         </p>
         <Button as={AnchorLink} href="#contact">
           Hire me
